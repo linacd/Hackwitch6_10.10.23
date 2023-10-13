a67920978cc770b791a0f7a62a607f619e601778
linacd <linacd@hawaii.edu>
2023-10-12 16:37:16 -1000

    Completed problem set #1

--- Hackwitch6_10.10.23/secondViewController.swift
+++ Hackwitch6_10.10.23/secondViewController.swift
@@ -7,23 +7,35 @@
 
 import UIKit
 
-class secondViewController: UIViewController {
+class secondViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var myPlacesArray = ["Japan", "Italy", "Canada"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myPlacesArray.count
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let text = myPlacesArray[indexPath.row]
+        cell.textLabel?.text = text
+        return cell
+        
+    }
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+       
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
